27ea1179a5379865ab49da30ac702eced92ab1f7
Anbarasan <90215292+anbu2014@users.noreply.github.com>
2022-02-01 00:01:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+import AllStudents from "./components/AllStudents";
+import AddStudents from "./components/AddStudents";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EditStudent from "./components/EditStudent";
+import { useState } from "react";
 function App() {
+  let data = {
+    earning: "40,000",
+    annual: "2,40,000",
+    task: 20,
+    pending: 26,
+  };
+
+  let [students, setStudents] = useState([
+    {
+      name: "Kiran",
+      email: "kiran@gmail.com",
+      mobile: "12345678",
+      class: "B30WD",
+    },
+    {
+      name: "Raj",
+      email: "Raj@gmail.com",
+      mobile: "45678123",
+      class: "B20WE",
+    },
+    {
+      name: "Hemant",
+      email: "hemant@gmail.com",
+      mobile: "12345678",
+      class: "B30WD",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div >
+          <div class="container">
+            <div class="row">
+              <div class="col-4">
+                <Sidebar />
+              </div>
+
+              <div class="col-8" >
+                <Routes>
+                  <Route
+                    path="/dashboard"
+                    element={<Dashboard data={data} />}
+                  />
+                  <Route
+                    path="/all-students"
+                    element={<AllStudents data={{ students, setStudents }} />}
+                  />
+                  <Route
+                    path="/add-student"
+                    element={<AddStudents data={{ students, setStudents }} />}
+                  />
+                  <Route
+                    path="/edit-student/:id"
+                    element={<EditStudent data={{ students, setStudents }} />}
+                  />
+                  <Route path="/" element={<Dashboard data={data} />} />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
